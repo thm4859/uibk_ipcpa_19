2d14abc4bd503c3340877594fcbc994baf174552
cliffhanger <thomas.urban@student.uibk.ac.at>
2019-04-11 21:51:43 +0200

    collect_samples.sh update

--- week6/for_csv/collect_samples.sh
+++ week6/for_csv/collect_samples.sh
@@ -5,7 +5,7 @@ I=0
              echo The counter is $COUNTER
              #while [ $I -lt 5 ]; do
              	./count_seq $COUNTER 42 >> stats_GT_730_seq.csv
-             	#./counter_ocl $COUNTER >> stats_GT_730_ocl.csv
+             	./faulty_count_ocl $COUNTER >> stats_GT_730_ocl.csv
              	./count_omp $COUNTER 42 >> stats_GT_730_omp.csv
 
              let COUNTER=COUNTER*10
